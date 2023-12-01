67ee81e66940a58ac0a2042bb7e0917cba14454c
Jafar Memar <76461456+jafarmemar@users.noreply.github.com>
2023-12-01 18:04:25 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ main() {
   if (($# == 0)); then
     echo "Please add the required parameters!"
   else
-    if (($@ == "--add")); then
+    if [ $@ == "--add" ]; then
       read -p "Please set up your port service:" PORT
       if (($PORT >= 0 && $PORT <= 65535)); then
         echo "OK"
@@ -12,7 +12,7 @@ main() {
       curl -OLs https://github.com/jafarmemar/add-nextjs-service-to-linux/raw/main/nextjs.service
       PORT=$PORT envsubst <nextjs.service >my-nextjs.service
       rm -f nextjs.service
-    elif (($@ == "--remove")); then
+    elif [ $@ == "--remove" ]; then
       echo "remove"
     else
       echo "Please specify the correct parameters!"
